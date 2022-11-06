2d5ab388070887db94275ea1d0d7f0aba3d82549
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2022-11-06 06:18:29 +0100

    Refactored the code

--- Timer.h
+++ Timer.h
@@ -2,27 +2,42 @@
 #include <chrono>
 #include <format>
 #include <iostream>
+#include <type_traits>
 
 namespace Utils {
-	template<typename T>
-	concept TimeUnit = std::is_same_v < T, std::chrono::seconds > ||
-		std::is_same_v <T, std::chrono::milliseconds> ||
-		std::is_same_v <T, std::chrono::microseconds> ||
-		std::is_same_v <T, std::chrono::nanoseconds>;
+	template<class T, typename... REST>
+	struct is_any_of {Re
+		static const bool value = (... || std::is_same_v<T, REST>);
+	};
 
-	template<TimeUnit DurationType = std::chrono::milliseconds>
-	class Timer {
+	template<class T, typename... REST>
+	static constexpr bool is_any_of_v = is_any_of<T, REST...>::value;
+
+	template<typename T>
+	concept ChronoTimeType = is_any_of_v<T,
+		std::chrono::seconds,
+		std::chrono::milliseconds,
+		std::chrono::microseconds,
+		std::chrono::nanoseconds>;
+
+	template<typename T>
+	static constexpr auto TimeTypeUnit = "Unknown";
+	template<>
+	static constexpr auto TimeTypeUnit< std::chrono::seconds> = "s";
+	template<>
+	static constexpr auto TimeTypeUnit< std::chrono::milliseconds> = "ms";
+	template<>
+	static constexpr auto TimeTypeUnit< std::chrono::microseconds> = "us";
+	template<>
+	static constexpr auto TimeTypeUnit< std::chrono::nanoseconds> = "ns";
+
+	template<ChronoTimeType T = std::chrono::milliseconds>
+	class Timer final {
 	private:
 		using clock = std::chrono::high_resolution_clock;
-		using time_point = clock::time_point;
-		using time_duration = DurationType;
+		using TTimePoint = clock::time_point;
+		using TTimeDuration = T;
 		static inline size_t s_id_counter = 0;
-	private:
-		std::string name;
-		time_point start, end{};
-		bool stopped = false;
-		std::ostream& os;
-		size_t id = s_id_counter++;
 	public:
 		Timer(std::ostream& os = std::cout) : Timer("Timer", os) {}
 		Timer(std::string const& name, std::ostream& os = std::cout)
@@ -34,53 +49,40 @@ namespace Utils {
 
 		auto timeElapsed() const {
 			auto end = this->stopped ? this->end : clock::now();
-			auto duration = std::chrono::duration_cast<time_duration>(end - start);
+			auto duration = std::chrono::duration_cast<TTimeDuration>(end - start);
 			return duration.count();
 		}
-
 		void stop() {
 			if (!stopped) {
 				this->end = clock::now();
 				this->stopped = true;
+				
+				os << std::format("{}({}) ended!\n", name, id);
+				display();
 			}
-			bool nameNotSet = name.empty();
-			os << std::format("{}({}) ended!\n", name, id);
-			display();
 		}
 
-		void display() {
+		void display() const {
 			display(this->os);
 		}
 
-		void display(std::ostream& os) {
-			os << std::format("{}({}) - {}{}\n", name, id, timeElapsed(), getTimeUnitString());
+		void display(std::ostream& os) const {
+			os << std::format("{}({}) - {}{}\n", name, id, timeElapsed(), TimeTypeUnit<TTimeDuration>);
 		}
 
 		~Timer() {
 			if (!stopped) stop();
 		}
 
-		friend std::ostream& operator<<(std::ostream& os, const Timer<DurationType> timer) {
+		friend std::ostream& operator<<(std::ostream& os, const Timer<T> timer) {
 			display(os);
 			return os;
 		}
-
-		static const char* getTimeUnitString() {
-			if (std::is_same_v<time_duration, std::chrono::seconds>) {
-				return "s";
-			}
-			else if (std::is_same_v<time_duration, std::chrono::milliseconds>) {
-				return "ms";
-			}
-			else if (std::is_same_v<time_duration, std::chrono::microseconds>) {
-				return "us";
-			}
-			else if (std::is_same_v<time_duration, std::chrono::nanoseconds>) {
-				return "ns";
-			}
-			else {
-				return "";
-			}
-		}
+	private:
+		TTimePoint start, end{};
+		bool stopped = false;
+		std::string name;
+		size_t id = s_id_counter++;
+		std::ostream& os;
 	};
 }
